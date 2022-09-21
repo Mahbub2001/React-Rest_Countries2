80f757114474763f2f130bde2babba19edb30cc3
Mahbub2001 <imahbubturza@gmail.com>
2022-09-22 00:20:50 +0600

    Way-1,2,3

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -13,18 +13,55 @@ const Countries = () => {
     return (
         <div>
            <h1>Hello from Countries :{countries.length}</h1> 
-           {
+           {/* {
                countries.map(country => 
                <Country 
-                name={country.name} 
-                capital={country.capital} 
-                population={country.population}
-                area = {country.area}
+                country = {country}
                 >
                 </Country>)
+           } */}
+           {
+            countries.map(country => 
+            <Country country={country}></Country>)
            }
         </div>
     );
 };
 
 export default Countries;
+
+
+//Previous Way ---------------------
+
+// import React, { useEffect, useState } from 'react';
+// import Country from '../Country/Country';
+// import './Countries.css';
+// const Countries = () => {
+//     const [countries,setCountries] = useState([])
+
+//     useEffect( ()=>{
+//         fetch('https://restcountries.com/v2/all')
+//         .then(res =>res.json())
+//         .then(data => setCountries(data));
+//     },[])
+
+//     return (
+//         <div>
+//            <h1>Hello from Countries :{countries.length}</h1> 
+//            {
+//                countries.map(country => 
+//                <Country 
+//                 country = {country}//
+//                 name={country.name} 
+//                 capital={country.capital} 
+//                 population={country.population}
+//                 area = {country.area}
+//                 region={country.region}
+//                 >
+//                 </Country>)
+//            }
+//         </div>
+//     );
+// };
+
+// export default Countries;
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,14 +1,62 @@
-import React from 'react';
-
+import React from "react";
+import "./Country.css";
 const Country = (props) => {
-    return (
-        <div>
-            <h2>Country Name : {props.name} </h2>
-            <p>capital Name : {props.capital}</p>
-            <p>Population : {props.population}</p>
-            <p>Area : {props.area} sq.km</p>
-        </div>
-    );
+    //advance way
+    const {area,region,population,name,capital} = props.country;
+  return (
+    <div className="country">
+      <h2>Country Name : {name} </h2>
+      <p>capital Name : {capital}</p>
+      <p>Population : {population}</p>
+      <p>Area : {area} sq.km</p>
+      <p>Region : {region} </p>
+    </div>
+  );
 };
 
 export default Country;
+
+
+
+
+
+
+
+//previous way------------
+
+// import React from "react";
+// import "./Country.css";
+// const Country = (props) => {
+//     const {area} = props.country;
+//   return (
+//     <div className="country">
+//       <h2>Country Name : {props.country.name} </h2>
+//       <p>capital Name : {props.country.capital}</p>
+//       <p>Population : {props.country.population}</p>
+//       <p>Area : {props.country.area} sq.km</p>
+//       <p>Region : {props.country.region} </p>
+//     </div>
+//   );
+// };
+
+// export default Country;
+
+
+
+//Previous previous Way----------------
+// import React from 'react';
+// import './Country.css'
+// const Country = (props) => {
+//     return (
+//         <div className='country'>
+//             <h2>Country Name : {props.name} </h2>
+//             <p>capital Name : {props.capital}</p>
+//             <p>Population : {props.population}</p>
+//             <p>Area : {props.area} sq.km</p>
+//             <p>Region : {props.region} </p>
+
+//         </div>
+//     );
+// };
+
+// export default Country;
